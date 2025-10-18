0191ba080bdacf28fc8ef910a72d6f9f68b09f22
filipm55 <filipmohler10@gmail.com>
2025-10-18 17:46:01 +0200

    Update formValidation.js
    Add validation for whole numbers

--- src/public/scripts/formValidation.js
+++ src/public/scripts/formValidation.js
@@ -8,24 +8,30 @@ function validateForm() {
         .map(n => n.trim())
         .filter(n => n !== '')
         .map(Number);
-
+        
     let valid = true;
-
-    if (card_id.length > 20) {
-        document.getElementById("cardError").textContent = "Card ID must be less than 20 characters.";
-        valid = false;
-    } else if (isNaN(card_id)) {
+    const regex = /[!@#$%^&*()\-+={}[\]:;"'<>,.?\/|\\]/;
+    if(regex.test(card_id) || isNaN(card_id)) {
         document.getElementById("cardError").textContent = "Card ID can only contain digits.";
         valid = false;
     }
-
+    else if (card_id.length > 20) {
+        document.getElementById("cardError").textContent = "Card ID must be less than 20 characters.";
+        valid = false;
+    }
     if (round_numbers.length < 6 || round_numbers.length > 10) {
         document.getElementById("numbersError").textContent = "You must enter between 6 and 10 numbers separated by commas.";
         valid = false;
     } else {
         for (let i = 0; i < round_numbers.length; i++) {
+            
             if (isNaN(round_numbers[i]) || round_numbers[i] < 1 || round_numbers[i] > 45) {
                 document.getElementById("numbersError").textContent = "Numbers must be between 1 and 45.";
+                valid = false;
+                break;
+            }
+            if (!Number.isInteger(round_numbers[i])) {
+                document.getElementById("numbersError").textContent = "Can only contain whole numbers.";
                 valid = false;
                 break;
             }
